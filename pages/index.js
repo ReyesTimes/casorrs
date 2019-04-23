d6a6f678f94fe0a1cab12d855a025b4da10fada0
reyestimes <rated-mvp@hotmail.com>
2019-04-23 14:43:41 -0500

    addrepositorie

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import '../sass/index.scss'
+
+const Index = () => (
+    <main className="container">
+        <article>
+            <section>
+                <h1>
+                    Investigación SSR Despacho
+                </h1>
+                <p>Se generara una investigación del caso <span className="underline">SSR Despacho</span> con el fin de obtener hallazgos que nos ayuden a diseñar la mejor solución para nuestros clientes.</p>
+                <p>Dicha investigación sera documentada en este sitio web.</p>
+            </section>
+            <section>
+                <h2>Diferenciador de SSR</h2>
+            </section>
+        </article>
+    </main>
+)
+  
+export default Index
