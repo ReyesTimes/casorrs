8c54f20d89ac69e29ea16c4018f4dd10d417d7df
reyestimes <rated.mvp1@gmail.com>
2019-05-29 11:51:34 -0500

    actualizar

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ab5081b13a02e837d73f";
+/******/ 	var hotCurrentHash = "5d87dd9f6a4bcfeb6a1c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
